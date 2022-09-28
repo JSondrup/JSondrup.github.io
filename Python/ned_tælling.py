59c9ed329a96db6fe39d8a3ab61287e0b253dde4
Katrine <katr131c@eucnordvest.dk>
2022-09-28 22:26:40 +0200

    Ny kode

---
+++ Python/ned_tælling.py
@@ -0,0 +1,3 @@
+#(a,b,c) a = start nr b = slut nr(ikke indkluderet) c = trin den tæller
+for i in range(10,0,-1):
+    print(i)
---
+++ Python/tabeller.py
@@ -0,0 +1,5 @@
+for t in range(11):
+    print(f"{t}-tabellen")
+    tabel = t
+    for i in range(1, 11):
+        print(str(tabel) + " x " + str(i) + " = " + str(tabel*i))
